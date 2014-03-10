6e8e228d83a6a1243b95443eacdc2e05d2f2ea1f
l00s3s <rbechtel.ostmann@googlemail.com>
2014-03-10 08:48:59 +0100

    AI algorithmen update von Christoph hinzugefügt (easy + inbtermediate)
    Signed-off-by: l00s3s <rbechtel.ostmann@googlemail.com>

--- nimich/nimEngine/AI/AI_easy.cs
+++ nimich/nimEngine/AI/AI_easy.cs
@@ -8,24 +8,49 @@ namespace nimEngine.AI
     public class AI_easy : Player
     {
     	Random rnd = new Random();
-    	
+
 		public override int Turn(int currentStickCount)
 		{
-			if(currentStickCount > 5)
-			{
-				return rnd.Next(1,3); 
-			}
-			else
-			{
-				if(currentStickCount < 4)
-				{
-					return rnd.Next(1,(currentStickCount + 1));
-				}
-				else
-				{
-					return rnd.Next(1,4);
-				}
-			}
+            if (currentStickCount > 5)
+            {
+                return rnd.Next(1, 4);
+            }
+            else if (currentStickCount == 5)
+            {
+                return rnd.Next(1, 4);
+            }
+
+            else if (currentStickCount == 4)
+            {
+                return rnd.Next(1, 3);
+            }
+            else if (currentStickCount == 3)
+            {
+                if (rnd.Next(10) >= 2)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 2;
+                }
+            }
+
+            else if (currentStickCount == 2)
+            {
+                if (rnd.Next(10) > 1)
+                {
+                    return 2;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+            else
+            {
+                return 1;
+            }
 		}
     }
 }
--- nimich/nimEngine/AI/AI_intermediate.cs
+++ nimich/nimEngine/AI/AI_intermediate.cs
@@ -9,7 +9,65 @@ namespace nimEngine.AI
     {
 		public override int Turn(int currentStickCount)
 		{
-			throw new NotImplementedException();
+            Random rnd = new Random();
+            int n = 0;
+            int temporary = 0;
+
+            if (currentStickCount > 5)
+            {
+                if (rnd.Next(10) >= 5)
+                {
+                    for (int i = 1; i < 1000; i++)
+                    {
+                        n++;
+                        temporary = 5 + (4 * n);
+
+                        if (temporary >= currentStickCount)
+                        {
+                            i = 1000;
+                        }
+
+                    }
+                    if (temporary == currentStickCount)
+                    {
+                        return rnd.Next(1, 4);
+                    }
+
+                    else if (temporary > currentStickCount)
+                    {
+                        if (rnd.Next(10) > 7)
+                        {
+                            temporary = temporary - currentStickCount;
+
+                            if (temporary == 3)
+                            {
+                                return 1;
+                            }
+
+                            if (temporary == 2)
+                            {
+                                return 2;
+                            }
+
+                            else
+                            {
+                                return 3;
+                            }
+                        }
+
+                        else
+                        {
+                            return rnd.Next(1, 4);
+                        }
+                    }
+                }
+
+                else
+                {
+                    return rnd.Next(1, 4);
+                }
+            }
+
 		}
     }
 }
