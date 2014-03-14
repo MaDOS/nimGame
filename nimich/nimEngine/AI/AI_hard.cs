e415042c600cbe7e397af26ef98505eaf498ee84
LittleChocobo <christophkinn@web.de>
2014-03-14 12:32:05 +0100

    AIs rdy

--- nimich/nimEngine/AI/AI_hard.cs
+++ nimich/nimEngine/AI/AI_hard.cs
@@ -9,7 +9,72 @@ namespace nimEngine.AI
     {
 		public override int Turn(int currentStickCount)
 		{
-			throw new NotImplementedException();
+            Random rnd = new Random();
+            int n = 0;
+            int temporary = 0;
+
+            if (currentStickCount > 5)
+            {
+                if (rnd.Next(20) >= 2)
+                {
+                    for (int i = 1; i < 1000; i++)
+                    {
+
+                        temporary = 5 + (4 * n);
+                        n++;
+
+                        if (temporary >= currentStickCount)
+                        {
+                            i = 1000;
+                        }
+                    }
+
+                    if (temporary == currentStickCount)
+                    {
+                        return 1;
+                    }
+
+                    else if (temporary > currentStickCount)
+                    {
+                        temporary = temporary - currentStickCount;
+
+                        if (temporary == 3)
+                        {
+                            return 1;
+                        }
+
+                        if (temporary == 2)
+                        {
+                            return 2;
+                        }
+
+                        else
+                        {
+                            return 3;
+                        }
+
+                    }
+                    else
+                    {
+                        return rnd.Next(1, 4);
+                    }
+                }
+                else
+                {
+                    return rnd.Next(1, 4);
+                }
+            }
+
+            else if (temporary == 5)
+            {
+                return 1;
+            }
+
+            else
+            {
+                return 1;
+            }
+                    
 		}
     }
 }
