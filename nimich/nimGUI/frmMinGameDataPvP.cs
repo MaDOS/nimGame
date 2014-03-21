a14c186d82b23067b946f9748c657bbaf0a86167
Vitamin0C <stefanfrank2@web.de>
2014-03-21 12:38:06 +0100

    GUI lokales Spiel fertig

--- nimich/nimGUI/frmMinGameDataPvP.cs
+++ nimich/nimGUI/frmMinGameDataPvP.cs
@@ -9,33 +9,12 @@ using System.Windows.Forms;
 
 namespace nimGUI
 {
-    public partial class frmNimGame : Form
+    public partial class frmNimGame
     {
-        public frmNimGame()
-        {
-            InitializeComponent();
-        }
-
-        private void butStartGame_Click(object sender, EventArgs e)
-        {
-            //Öffnen der Spielmodiauswahl
-            tabMain.SelectedTab = tbPgGameMode;
-        }
-
-        private void butSettings_Click(object sender, EventArgs e)
-        {
-            //Einstellungen Öffnen
-        }
-
-        private void butHighscore_Click(object sender, EventArgs e)
-        {
-            //Highscore öffnen
-        }
-
         private void butStartHotSeat_Click(object sender, EventArgs e)
         {
             //Spielerdatenblatt öffnen
-            tabMain.SelectedTab = tbPgGameData1;
+            tabMain.SelectedTab = tbPgGamePvP;
         }
 
         private void butStartGame1_Click(object sender, EventArgs e)
@@ -43,11 +22,12 @@ namespace nimGUI
             string[] player = new string[2];
             string message1 = "Spieler1: Bitte geben Sie ihren Namen ein!";
             string message2 = "Spieler2: Bitte geben Sie ihren Namen ein!";
-            
+
             //Speichern der Spielerdaten
             if (txtPlayerName1.Text == "")
             {
-                MessageBox.Show(message1);                
+                MessageBox.Show(message1);
+                return;
             }
             else
             {
@@ -58,10 +38,10 @@ namespace nimGUI
                 MessageBox.Show(message2);
             }
             else
-            {                
+            {
                 player[1] = txtPlayerName2.Text;
             }
-            
+
             //Übernehmen der Daten
             labNameP1.Text = player[0];
             labNameP2.Text = player[1];
